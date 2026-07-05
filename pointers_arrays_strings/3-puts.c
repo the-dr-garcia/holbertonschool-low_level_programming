db7e6ce2dba2a0255ee5b73e0bd29c5c9cbb37d2
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-05 08:54:44 +0000

    Add 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: The pointer to the string to print.
+ *
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
