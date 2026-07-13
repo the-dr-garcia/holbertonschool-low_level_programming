2c68b4330e60764e17e2f391abab27e3d502fc6f
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-13 02:12:00 +0000

    Add dog.h script

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
