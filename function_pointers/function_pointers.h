9cc9b892488b5dcd3a71c0e76058f02028410032
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-19 23:04:29 +0000

    Add 1-array_iterator.c script

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,7 +5,10 @@
 #define NULL ((void *)0)
 #endif
 
+typedef unsigned long size_t;
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 
 #endif
