02a397c15b732a5302a498d4c1d752d0b7ce98e7
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-19 23:03:29 +0000

    Add NULL definition to function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 
