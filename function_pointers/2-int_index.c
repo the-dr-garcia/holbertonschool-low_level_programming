c7980c3d866af0a57ed4342060539b643dc26fdb
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-19 23:05:21 +0000

    Add 2-int_index.c script

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: the array to search
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: index of the first element for which cmp doesn't return 0,
+ * or -1 if no element matches or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,6 +7,7 @@
 
 typedef unsigned long size_t;
 
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
