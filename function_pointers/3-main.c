e6cb9cf8445fa30e5b64c58a0b200b709f68971d
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-19 23:06:45 +0000

    Add 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h scripts

---
+++ function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int (*op_func)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op_func = get_op_func(argv[2]);
+	if (!op_func || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,7 @@
+#include "calc.h"
+
+int op_add(int a, int b) { return (a + b); }
+int op_sub(int a, int b) { return (a - b); }
+int op_mul(int a, int b) { return (a * b); }
+int op_div(int a, int b) { return (a / b); }
+int op_mod(int a, int b) { return (a % b); }
