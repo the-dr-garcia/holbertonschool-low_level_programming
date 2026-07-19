5a7a0d5852cfd05c99c292f26673510aea3eaa9f
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-07-19 23:39:45 +0000

    .

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,10 +3,22 @@
 
 #include <stdarg.h>
 
-void print_all(const char * const format, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
+
+/**
+ * struct printer - structure for printing types
+ * @c: the character representing the type
+ * @f: the function to handle the printing
+ */
+typedef struct printer
+{
+	char c;
+	void (*f)(va_list);
+} printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
