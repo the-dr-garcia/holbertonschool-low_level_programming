f9cbaf81f3ae9b97660a3ef47fdbb2e91455ba78
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-08-02 09:12:38 +0000

    Add 3-hash_table_set.c script

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,37 @@
 #include "hash_tables.h"
 
 /**
+ * make_hash_node - creates a new hash node
+ * @key: the key
+ * @value: the value
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	return (new);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table you want to add or update the key/value to
  * @key: the key, cannot be an empty string
@@ -34,24 +65,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	}
 
-	new = malloc(sizeof(hash_node_t));
+	new = make_hash_node(key, value);
 	if (new == NULL)
 		return (0);
-
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
 
 	new->next = ht->array[index];
 	ht->array[index] = new;
