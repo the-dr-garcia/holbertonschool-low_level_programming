8e58d08703ee2d128d4bf5fafbe650aee532478d
Daniel Garcia <daniel.ivan.garcia.aguilar@gmail.com>
2026-08-10 02:48:31 +0000

    Add 3-cp.c script

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,6 +17,37 @@ void close_file(int fd)
 }
 
 /**
+ * copy_file_content - Copies content from source to destination file descriptors.
+ * @fd_from: The source file descriptor.
+ * @fd_to: The destination file descriptor.
+ * @argv: Argument vector for error messages.
+ */
+void copy_file_content(int fd_from, int fd_to, char *argv[])
+{
+	ssize_t n_read, n_written;
+	char buf[1024];
+
+	while ((n_read = read(fd_from, buf, 1024)) > 0)
+	{
+		n_written = write(fd_to, buf, n_read);
+		if (n_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(99);
+		}
+	}
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_file(fd_from);
+		close_file(fd_to);
+		exit(98);
+	}
+}
+
+/**
  * main - Copies the content of a file to another file.
  * @argc: The number of arguments.
  * @argv: The array of arguments.
@@ -26,8 +57,6 @@ void close_file(int fd)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t n_read, n_written;
-	char buf[1024];
 
 	if (argc != 3)
 	{
@@ -47,24 +76,7 @@ int main(int argc, char *argv[])
 		close_file(fd_from);
 		exit(99);
 	}
-	while ((n_read = read(fd_from, buf, 1024)) > 0)
-	{
-		n_written = write(fd_to, buf, n_read);
-		if (n_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_file(fd_from);
-			close_file(fd_to);
-			exit(99);
-		}
-	}
-	if (n_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close_file(fd_from);
-		close_file(fd_to);
-		exit(98);
-	}
+	copy_file_content(fd_from, fd_to, argv);
 	close_file(fd_from);
 	close_file(fd_to);
 	return (0);
